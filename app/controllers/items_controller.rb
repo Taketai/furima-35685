fa144f913f1ad26a7f84c54e8883517727374483
Taketai <83339671+Taketai@users.noreply.github.com>
2021-05-24 19:40:55 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,9 @@ class ItemsController < ApplicationController
     @items = Item.order("created_at DESC")
   end
 
-  # def show
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
   def new
      @item = Item.new
